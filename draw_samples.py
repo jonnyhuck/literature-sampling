f59c69d9b0833777f6188dc3d4b307e9a9553c9c
Jonny Huck <jonnyhuck@gmail.com>
2020-09-14 17:26:33 +0100

    initial commit

---
+++ draw_samples.py
@@ -0,0 +1,27 @@
+from pandas import read_excel, concat
+
+# setup the input data. extra forces additional samples if required (e.g. fulltext is not available)
+datasets = [
+    {'filename':'./data/Mental Mapping Numbered.xlsx',  'n': 85,    'extra': 0},
+    {'filename':'./data/PGIS Numbered.xlsx',            'n': 243,   'extra': 2},
+    {'filename':'./data/Sketch Mapping Numbered.xlsx',  'n': 33,    'extra': 0},
+    {'filename':'./data/Unclassified Numbered.xlsx',    'n': 179,   'extra': 2},
+    ]
+
+# loop through each dataset
+samples = []
+for d in datasets:
+
+    # open data files
+    df = read_excel(d['filename'])
+
+    # calculate sample size (23%)
+    sample_size = int(round(d['n'] * 0.2)) + d['extra']
+
+    # extract random sample (set seed for reproducibility)
+    samples.append(df.sample(n=sample_size, random_state=1824))
+
+# concatenate the results into a single dataframe and write to file
+concat(samples).to_csv("./sample.csv")
+
+print("done!")
